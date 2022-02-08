5d18e2c61b00372469e9570dbc6387e777a29ca1
Marcelo Valentim <vdomarcelo@gmail.com>
2022-02-07 22:04:23 -0300

    initial commit with Electron first app and renderer test

---
+++ renderer.js
@@ -0,0 +1,8 @@
+const eName = document.querySelector("#electron");
+
+const colors = ["red", "orange", "purple", "blue", "pink", "brown", "violet"];
+
+eName.addEventListener("click", () => {
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  eName.style.color = color;
+});
